8a131826a3c9546d8afef8c346785e1242d81259
Dan Manning <dmanning@oceaneering.com>
2024-10-16 10:49:41 -0500

    initial commit

---
+++ MauiAppListenableFutureBug/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppListenableFutureBug
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
